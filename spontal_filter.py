4e8a10d375d5accd2cda882806a852bfbc35e30d
Marlies <liezq@live.nl>
2015-02-25 08:33:28 +0100

    filter data

--- spontal_filter.py
+++ spontal_filter.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
 import sys
+from xml.dom import minidom
+
+def main():
+    doc = minidom.parse("spontal.xml")
+    name = doc.getElementsByTagName("FO_START")[0]
+    print(name.firstChild.data)
+
+
+main()
 
