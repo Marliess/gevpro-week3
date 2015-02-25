d0c3957aa583bfd5093f1bad580682632513c625
Marliess <liezq@live.nl>
2015-02-25 10:43:41 +0100

    Blood die json

---
+++ blood_die.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+"""
+1. .json inlezen
+2. lijst maken van alle talen en hun classificaties waar tenminste
+    een van de woorden voor "bloed" identiek is aan een van de
+    woorden voor "sterven"
+3. List comprehension gebruiken voor lijst van resultaten
+4. elk element in de lijst moet een named tuple zijn
+
+Bestand openen: json.load()
+Bestand opslaan: json.dump()
+
+"""
+
+
+    
