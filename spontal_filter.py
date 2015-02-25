0984f5214fd9e1cd59c98ba6b960a37c8213404f
Marliess <liezq@live.nl>
2015-02-25 10:42:54 +0100

    Filter data

--- spontal_filter.py
+++ spontal_filter.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
 
 import sys
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
+
+"""
+1. corpus inlezen
+2. datapoints controleren
+3. foutieve datapoints verwijderen
+4. corpus naar bestand schrijven
+"""
 
 def main():
-    doc = minidom.parse("spontal.xml")
-    name = doc.getElementsByTagName("FO_START")[0]
-    print(name.firstChild.data)
+    tree = ET.parse("spontal.xml")
+    root = tree.getroot()
+    for child in root:
+        FO_END = child[12].text
+        FO_START = child[13].text
+        TOP_HZ = child[7].text
+        BOTTOM_HZ = child[5].text
+        if FO_END > TOP_HZ and FO_END < BOTTOM_HZ:
+            root.remove("FO_END")
+        else:
+            tree.write("spontal_filtered.xml")
+        if FO_START < BOTTOM_HZ and FO_START > TOP_HZ:
+            root.remove("FO_START")
+        else:
+            tree.write("spontal_filtered.xml")
 
 
-main()
+if __name__ == "__main__":
+    main()
 
